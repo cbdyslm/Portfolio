46e549b8e628f9d09057bcd096bafc222132fa4b
cbdyslm8 <ayusufm@outlook.com>
2024-11-04 22:01:56 +0300

    11/4/2024 work

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { logo } from "../assets";
 
 const NavBar = () => {
+  const [toggle, setToggle]=useState(true)
+
   return (
     <div>
-      <div>
+      <div className="flex justify-between">
         <img src={logo} className="h-5" />
-        <div>
-          <ul>
-            <li>Home</li>
-            <li>About Me</li>
-            <li>Skills</li>
-            <li>Portfolio</li>
-            <li>Companies</li>
-            <li>Contact Me</li>
+        <div className="hidden md:flex">
+          <ul className="flex flex-row">
+            <li className="mr-4 transition-all east-in-out hover:scale-110 cursor-pointer">Home</li>
+            <li className="mr-4 transition-all east-in-out hover:scale-110 cursor-pointer">About Me</li>
+            <li className="mr-4 transition-all east-in-out hover:scale-110 cursor-pointer">Skills</li>
+            <li className="mr-4 transition-all east-in-out hover:scale-110 cursor-pointer">Portfolio</li>
+            <li className="mr-4 transition-all east-in-out hover:scale-110 cursor-pointer">Companies</li>
+            <li className="mr-4 transition-all east-in-out hover:scale-110 cursor-pointer">Contact Me</li>
           </ul>
         </div>
+
+        <div>
+        {!toggle?"Toggle is Off":"Toggle is On"}
+        </div>
+        
       </div>
     </div>
   );
