13639a4c46ca97561ec79a0c1bd1bdcc90b11b44
cbdyslm8 <ayusufm@outlook.com>
2024-11-05 21:17:08 +0300

    work 11/5/2024

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { logo } from "../assets";
 
 const NavBar = () => {
-  const [toggle, setToggle]=useState(true)
+  const [toggle, setToggle]=useState(false)
 
   return (
     <div>
@@ -19,8 +19,31 @@ const NavBar = () => {
           </ul>
         </div>
 
-        <div>
-        {!toggle?"Toggle is Off":"Toggle is On"}
+        <div className="md:hidden lg:hidden">
+        {!toggle?
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+        onClick={()=>setToggle(true)}
+        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 cursor-pointer">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+      </svg>
+      :
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+        onClick={()=>setToggle(false)}
+        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 cursor-pointer float-right">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+</svg>
+}
+{toggle?
+  <ul className="flex flex-col mt-8 bg-gray-300 p-2">
+            <li className="p-3 transition-all east-in-out hover:scale-110 cursor-pointer">Home</li>
+            <li className="p-3 transition-all east-in-out hover:scale-110 cursor-pointer">About Me</li>
+            <li className="p-3 transition-all east-in-out hover:scale-110 cursor-pointer">Skills</li>
+            <li className="p-3 transition-all east-in-out hover:scale-110 cursor-pointer">Portfolio</li>
+            <li className="p-3 transition-all east-in-out hover:scale-110 cursor-pointer">Companies</li>
+            <li className="p-3 transition-all east-in-out hover:scale-110 cursor-pointer">Contact Me</li>
+          </ul>
+          :
+null}
         </div>
         
       </div>
